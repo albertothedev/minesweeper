664c6ada9b845970581e450c028fca67520e90ac
Alberto Vilches <albertothedev@protonmail.com>
2023-01-11 10:09:17 +0100

    ref: improve the typing of Redux's reducers
    resolves #51

--- redux/index.ts
+++ redux/index.ts
@@ -17,13 +17,15 @@ const rootSlice = createSlice({
   name: "rootSlice",
   initialState,
   reducers: {
-    setDataFetched: (state, action: PayloadAction<boolean>) =>
-      void (state.dataFetched = action.payload),
-    setGames: (state, action: PayloadAction<Array<TGame>>) =>
+    setDataFetched: (
+      state,
+      action: PayloadAction<TInitialState["dataFetched"]>
+    ) => void (state.dataFetched = action.payload),
+    setGames: (state, action: PayloadAction<TInitialState["games"]>) =>
       void (state.games = action.payload),
-    addGame: (state, action: PayloadAction<TGame>) =>
+    addGame: (state, action: PayloadAction<TInitialState["games"][number]>) =>
       void state.games.push(action.payload),
-    setUsername: (state, action: PayloadAction<string | undefined>) =>
+    setUsername: (state, action: PayloadAction<TInitialState["username"]>) =>
       void (state.username = action.payload),
   },
 });
